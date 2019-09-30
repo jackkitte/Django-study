fcf2297b8dcad8b2b43b41b3568019698c7723fb
jackkitte <jackkitte735@gmail.com>
2019-09-30 16:01:26 +0900

    cssの修正&ルーティングの修正

--- drf-vue-sample/frontend/src/App.vue
+++ drf-vue-sample/frontend/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+main.container {
+  border: 1px solid #cccccc;
+  margin-top: 4em;
+  padding: 2em;
+  width: 600px;
 }
 </style>
