77df52ea3a5854d08be6f39525f8c7b203c4d39f
jackkitte <jackkitte735@gmail.com>
2019-10-06 00:04:53 +0900

    debug_toolbarの使用

--- drf-vue-sample/config/settings.py
+++ drf-vue-sample/config/settings.py
@@ -156,3 +156,17 @@ CORS_ORIGIN_ALLOW_ALL = True
 # )
 
 django_heroku.settings(locals())
+
+if DEBUG:
+    def show_toolbar(request):
+        return True
+
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+    MIDDLEWARE += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
